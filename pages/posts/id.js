a2e0c60196c7183fdc15a0303944db5e50435cee
Ikeno Sosuke <ikenosousuke@icloud.com>
2023-10-04 09:14:25 +0900

    first server

---
+++ pages/posts/id.js
@@ -0,0 +1,13 @@
+import Layout from '../../components/layout';
+
+export default function Post() {
+    return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+
+}
+
+export async function getStaticProps({ params }) {
+
+}
